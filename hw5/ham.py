ffdf7a7912e5d9d3cb4dad7d5a4722df1af72827
Wyrine <kashahat@gmail.com>
2018-04-10 17:57:18 -0400

    finished report, just need to finish problem 2

--- hw5/ham.py
+++ hw5/ham.py
@@ -6,25 +6,19 @@ from globalign import globalAlign
 
 def printTraceBack(seq1, seq2, tb, indexList):
 		i, j = indexList[0]
-		match = 0
 		total = 0
 		while i >= 1 and j >= 1:
 				#0 is left
 				if tb[i, j] == 0: j -= 1
 				#1 is up
 				elif tb[i, j] == 1: i -= 1
-				#2 is diagonal -- match
-				elif seq1[i-1] == seq2[j-1]:
-						i -= 1
-						j -= 1
-						match += 1
-						total += 1
-				#2 is diagonal -- mismatch
+				#2 is diagonal 
 				else:
+						if seq1[i-1] != seq2[j-1]:
+								total += 1
 						i -= 1
 						j -= 1
-						total += 1
-		return total - match
+		return total 
 
 def setupHamming():
 		with open(sys.argv[1]) as f:
@@ -55,7 +49,7 @@ if __name__ == "__main__":
 		hams = setupHamming()
 		print("\n  " + " ".join([str(i) for i in range(1, len(hams)+1)]))
 		for i in range(len(hams)):
-				print(str(i) + " ", end = "")
+				print(str(i+1) + " ", end = "")
 				for j in range(len(hams)):
 						print(hams[i, j], end=" ")
 				print()
