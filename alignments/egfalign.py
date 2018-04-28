d60c9bfc27525acc6d16db01f36d9697d285a1aa
Wyrine <kashahat@gmail.com>
2018-04-28 10:41:34 -0400

    fixed end gap free alignment

--- alignments/egfalign.py
+++ alignments/egfalign.py
@@ -2,57 +2,40 @@
 
 import numpy as np
 import sys
-from globalign import setupAlign
+
+def setupAlign(file1, file2, scoring, alignment):
+		"""
+        scoring is defined as: 
+        scoring[0] = match
+        scoring[1] = mismatch
+        scoring[2] = gap
+		"""
+		with open(file1) as f:
+				f.readline()
+				seq1 = f.read().replace("N", "A").replace("\n", "")
+		with open(file2) as f:
+				f.readline()
+				seq2 = f.read().replace("N", "A").replace("\n", "")
+		alignment(seq1, seq2, scoring)
     
 def gapFreeAlign(seq1, seq2, scoring):
-    #tb is traceback matrix
-    tb = np.zeros( (len(seq1) + 1, len(seq2)+1) , dtype=np.int ) 
-    #mat is the scores matrix
-    mat = np.zeros( (len(seq1) + 1, len(seq2)+1), dtype=np.int )
+		#mat is the scores matrix
+		mat = np.zeros( (len(seq1) + 1, len(seq2)+1), dtype=np.int )
 
-    #set up the matrix with gap scores in first row and first column
-    for i in range(1, len(seq1) + 1):
-        for j in range(1, len(seq2) + 1):
-            print(i, j) 
-            print(len(seq1), len(seq2))
-            #left up and diag are the three cases and thus represent the scores for each
-            left = mat[i, j-1] + scoring[2]
-            up = mat[i-1, j] + scoring[2]
-            diag = mat[i-1, j-1] + (scoring[0] if seq1[i-1] == seq2[j-1] else scoring[1])
-
-            #Which one gives the best score?
-            if left >= up and left >= diag:
-                tb[i,j] = 0 #0 is left as traceback
-                #best score is a gap from left
-                mat[i, j] = left
-            elif up >= left and up >= diag:
-                tb[i, j] = 1 #1 is up as traceback
-                #best score is a gap from up
-                mat[i, j] = up
-            else:
-                tb[i, j] = 2 #2 is diagonal as traceback
-                #best score is match/mismatch from diagonal
-                mat[i, j] = diag
-    maxVal, x, y = 0, 0, 0
-    #columns
-    for i in range(1, len(seq2)+1):
-        if mat[len(seq1), i] > maxVal:
-            maxVal, x, y = mat[len(seq1), i], len(seq1), i
-    #rows
-    for i in range(1, len(seq1)+1):
-        if mat[i, len(seq2)] > maxVal:
-            maxVal, x, y = mat[i, len(seq2)], i, len(seq2)
-
-    #return tb matrix, the matrix of scores, and a list of tuples of max score
-    return tb, mat, [(x, y)]
+		for i in range(1, len(seq1) + 1):
+				for j in range(1, len(seq2) + 1):
+						#left up and diag are the three cases and thus represent the scores for each
+						left = mat[i, j-1] + scoring[2]
+						up = mat[i-1, j] + scoring[2]
+						diag = mat[i-1, j-1] + (scoring[0] if seq1[i-1] == seq2[j-1] else scoring[1])
+						mat[i,j] = max(left, up, diag)
+		print("Optimal End Gap Free Score:", max(np.max(mat[len(seq1), :]) , np.max(mat[:, len(seq2)])))
 
 if __name__ == "__main__":
-    scoring = [2, -1, -2]
-    f1, f2 = "human.fasta", "fruit_fly.fasta"
-    if len(sys.argv) > 3:
-         scoring  = [int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])]
-    if len(sys.argv) > 1:
-        f1, f2 = str(sys.argv[1]), str(sys.argv[1])
-    tb, mat, indexList, seq1, seq2 = setupAlign(f1, f2, scoring, gapFreeAlign)
-    print("Optimal Score:", mat[len(seq1), len(seq2)])
-    
+		scoring = [2, -1, -2]
+		f1, f2 = "human_mito.fasta", "neander_sample.fasta"
+		if len(sys.argv) > 3:
+				scoring  = [int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])]
+		if len(sys.argv) > 1:
+				f1, f2 = str(sys.argv[1]), str(sys.argv[1])
+		setupAlign(f1, f2, scoring, gapFreeAlign)
